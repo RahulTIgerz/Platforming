ff5df2a579a147f9fd93355a125ac92c3b25b1c9
RahulTIgerz <rahrao83@kingshigh.school.nz>
2022-09-09 14:59:35 +1200

    cbuieanhfpac;x

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export (int) var speed = 120
+export (int) var speed = 180
 export (int) var jump_speed = -180 
 export (int) var gravity = 400
 export (int) var slide_speed = 400
